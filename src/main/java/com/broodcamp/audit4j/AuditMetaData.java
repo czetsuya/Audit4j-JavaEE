4cc753a7e308ba17934c8bc9664a466b269d7a7a
Edward P. Legaspi <czetsuya@gmail.com>
2017-04-28 15:48:34 +0800

    Using JNDI datasource.

--- src/main/java/com/broodcamp/audit4j/AuditMetaData.java
+++ src/main/java/com/broodcamp/audit4j/AuditMetaData.java
@@ -1,16 +1,41 @@
 package com.broodcamp.audit4j;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Context;
+
 import org.audit4j.core.MetaData;
 
 public class AuditMetaData implements MetaData {
 
+	@Context
+	private HttpServletRequest httpRequest;
+
 	@Override
 	public String getOrigin() {
-		return "localhost";
+		try {
+			return getClientIp(httpRequest);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "unidentified";
 	}
 
 	@Override
 	public String getActor() {
 		return "edward";
+	}
+
+	private static String getClientIp(HttpServletRequest request) {
+
+		String remoteAddr = "";
+
+		if (request != null) {
+			remoteAddr = request.getHeader("X-FORWARDED-FOR");
+			if (remoteAddr == null || "".equals(remoteAddr)) {
+				remoteAddr = request.getRemoteAddr();
+			}
+		}
+
+		return remoteAddr;
 	}
 }
